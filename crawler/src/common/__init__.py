d85dfffc0a06213b80a41c11db7d92382796d174
Takashi SASAKI <takashi316@gmail.com>
2012-06-13 19:51:52 +0900

    A singleton instance of SqlAlchemySessionFactory was instantiated in common/__init__.py

--- crawler/src/common/__init__.py
+++ crawler/src/common/__init__.py
@@ -1,7 +1,9 @@
+from __future__ import unicode_literals, print_function
 from com.gmail.takashi316.lib.sqlite import *
-from com.gmail.takashi316.lib.sqlalchemy import *
 from com.gmail.takashi316.lib.file import *
 from com.gmail.takashi316.lib.debug import *
 from com.gmail.takashi316.lib.datetime import * 
-from unittest import TestCase, main
 from ConfigParser import ConfigParser
+from com.gmail.takashi316.lib.sqlalchemy import *
+SqlAlchemySessionFactory("obomb", "v1")
+from unittest import TestCase
