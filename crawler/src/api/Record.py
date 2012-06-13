8be0dcd541873e9d744f72ba3c8e54c4f5e32a25
Takashi SASAKI <takashi316@gmail.com>
2012-06-13 23:54:41 +0900

    more strict unit test

--- crawler/src/api/Record.py
+++ crawler/src/api/Record.py
@@ -1,17 +1,18 @@
-from config import *
+from __future__ import unicode_literals, print_function
+__all__ = ["RecordApp"]
+from common import *
 from webapp2 import RequestHandler, WSGIApplication
 from paste.request import path_info_pop
-from lib.Record import Record
-from sqlalchemy.exc import OperationalError
+from lib.Record import *
+from lib.TableMixin import *
+from sqlalchemy.exc import *
 
-class _RecordHandler(RequestHandler):
+class _RecordHandler(RequestHandler, TableMixin):
     def get(self):
         self.response.out.write(self.request.path_info)
         if self.request.path_info == "":
-            session = Session()
             try:
-                data_table = Record.getGvizDataTable(session)
-                session.close()
+                data_table = Record.getGvizDataTable()
             except OperationalError, e:
                 self.response.set_status(404)
                 self.response.out.write(e.message)
@@ -28,15 +29,37 @@ class RecordApp(WSGIApplication):
         #info("PATH_INFO = " + environ["PATH_INFO"]) 
         return WSGIApplication.__call__(self, environ, start_response)
 
-class _Test(TestCase):
+class _(TestCase):
+    
     def setUp(self):
         TestCase.setUp(self)
-
-    def test(self):
-        record_app = RecordApp()
+        from random import randint
+        self.port = randint(10000, 20000)
+        from lib.WsgiRunner import PasteThread
+        self.pasteThread = PasteThread(RecordApp(), self.port, timeout=3)
+        self.pasteThread.start()
+        import time
+        time.sleep(1)
+        
+    def testGet(self):
+        self.assertTrue(self.pasteThread.isAlive())
+        from httplib import HTTPConnection
+        http_connection = HTTPConnection("localhost", port=self.port)
+        http_connection.request("GET", "/api/Record")
+        response = http_connection.getresponse()
+        debug(response.read())
+        self.assertTrue(response.status == 404 or response.status == 200 or response.status == 500)
+    
+    def testGvizDataTable(self):
+        session = SqlAlchemySessionFactory().createSqlAlchemySession()
+        import gviz_api
+        data_table = Record.getGvizDataTable()
+        self.assertIsInstance(data_table, gviz_api.DataTable)
+        import re
+        r = re.compile(r"^google\.visualization\.Query\.setResponse\({.*}\);$")
+        m = r.match(data_table.ToResponse())
+        self.assertIsNotNone(m)
+        session.close()
     
     def tearDown(self):
         TestCase.tearDown(self)
-        
-if __name__ == "__main__":
-    main()
