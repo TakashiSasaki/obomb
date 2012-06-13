112957868b7bc2a4b10190b4054d1236e19adbc2
Takashi SASAKI <takashi316@gmail.com>
2012-06-13 17:30:07 +0900

    class _XYZ for unit test in DeclarativeBase.py

--- crawler/src/lib/DeclarativeBase.py
+++ crawler/src/lib/DeclarativeBase.py
@@ -1,4 +1,49 @@
-from config import *
+from __future__ import unicode_literals, print_function
+from common import *
 from sqlalchemy import  MetaData as _MetaData
 from sqlalchemy.ext.declarative import declarative_base as _declarative_base
 DeclarativeBase = _declarative_base(metadata=_MetaData())
+from sqlalchemy import  Column, String, Integer, DateTime, Boolean
+DeclarativeBase = _declarative_base(metadata=_MetaData())
+
+class _XYZ(DeclarativeBase):
+    debug("XYZ start")
+    __tablename__ = "XYZ"
+    __table_args__ = {'sqlite_autoincrement': True}
+
+    c1 = Column(Integer, primary_key=True)
+    c2 = Column(String())
+    c3 = Column(DateTime)
+    c4 = Column(Boolean)
+
+    @classmethod
+    def dropTable(cls):
+        try:
+            table = DeclarativeBase.metadata.tables[cls.__tablename__]
+            engine = SqlAlchemyEngine().getEngine()
+            table.drop(engine, checkfirst=True)
+        except:
+            pass
+    
+    @classmethod
+    def createTable(cls):
+        try:
+            debug("try")
+            table = DeclarativeBase.metadata.tables[cls.__tablename__]
+            engine = SqlAlchemyEngine().getEngine()
+            table.create(engine, checkfirst=True)
+        except:
+            pass
+
+import unittest
+class _(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+        
+    def testDeclarativeBase(self):
+        debug("test start")
+        xyz = _XYZ()
+        _XYZ.dropTable()
